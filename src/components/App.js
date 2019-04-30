b0995730a9d4323e2680f758ca07d8c8241bca53
Luis Carlin <luiscarlin@gmail.com>
2019-04-29 20:07:53 -0400

    remove pattern

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,5 @@
 import React from 'react'
-import cogsSvg from '../assets/floating-cogs.svg'
-import './app.scss'
 
-console.log(cogsSvg)
-const App = () => {
-  const dataUri = `url("data:image/svg+xml,${encodeURIComponent(cogsSvg)}")`
-
-  return <div className="page-container">hello</div>
-}
+const App = () => hello
 
 export default App
