f14c0090656f671f13f9fd2a0d494688787b6453
Luis Carlin <luiscarlin@gmail.com>
2019-05-01 06:49:06 -0400

    lambda ready responding with joke and gif url

--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,27 @@ import React, { useEffect, useState } from 'react'
 import GphApiClient from 'giphy-js-sdk-core'
 
 function App() {
-  const client = GphApiClient('7wbcODKPVMUOFU45l5cOua3gpJKsvaAs')
+  const client = GphApiClient(process.env.GIPHY_KEY)
   const [joke, setJoke] = useState('')
   const [gif, setGif] = useState('')
 
   const getJoke = () => {
-    const url = 'https://icanhazdadjoke.com/'
-    const options = {
-      headers: { Accept: 'application/json' },
-    }
-
-    fetch(url, options)
-      .then(response => response.json())
-      .then(result => {
-        setJoke(result.joke)
-        return result.joke
-      })
-      .then(joke => {
-        client
-          .translate('gifs', { s: joke })
-          .then(response => setGif(response.data.embed_url))
-      })
-      .catch(error => console.log(`There was an error: ${error}`))
+    // const url = 'https://icanhazdadjoke.com/'
+    // const options = {
+    //   headers: { Accept: 'application/json' },
+    // }
+    // fetch(url, options)
+    //   .then(response => response.json())
+    //   .then(result => {
+    //     setJoke(result.joke)
+    //     return result.joke
+    //   })
+    //   .then(joke => {
+    //     client
+    //       .translate('gifs', { s: joke })
+    //       .then(response => setGif(response.data.embed_url))
+    //   })
+    //   .catch(error => console.log(`There was an error: ${error}`))
   }
 
   useEffect(() => {
--- src/lambda/getJoke.js
+++ src/lambda/getJoke.js
@@ -1,15 +1,32 @@
 const fetch = require('node-fetch').default
+const GphApiClient = require('giphy-js-sdk-core')
 
 const API_ENDPOINT = 'https://icanhazdadjoke.com/'
 
 exports.handler = function(event, context, callback) {
+  const client = GphApiClient(process.env.GIPHY_KEY)
+
+  const giphyPromise = jokeText => {
+    return new Promise((resolve, reject) => {
+      client
+        .translate('gifs', { s: jokeText })
+        .then(payload => resolve(payload.data.embed_url))
+        .catch(error => reject(error))
+    })
+  }
+
   fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
     .then(response => response.json())
-    .then(data =>
-      callback(null, {
-        statusCode: 200,
-        body: data.joke,
+    .then(data => data.joke)
+    .then(joke => {
+      giphyPromise(joke).then(gifUrl => {
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({
+            joke,
+            gifUrl,
+          }),
+        })
       })
-    )
-    .catch(error => ({ statusCode: 422, body: String(error) }))
+    })
 }
