6b8b325a76f0e6c730420ae15beb28f0f5bca44b
Luis Carlin <luiscarlin@gmail.com>
2019-05-01 06:11:47 -0400

    move back in netlify version because of issue with restarting on port 9000

--- src/lambda/getJoke.js
+++ src/lambda/getJoke.js
@@ -1,7 +1,14 @@
-export function handler(event, context, callback) {
-  console.log('queryStringParameters', event.queryStringParameters)
+const fetch = require('node-fetch').default
+
+exports.handler = function(event, context, callback) {
+  fetch('https://github.com/')
+    .then(res => res.text())
+    .then(body => console.log(body))
+  // your server-side functionality
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!' }),
+    body: JSON.stringify({
+      message: `Hello world ${Math.floor(Math.random() * 10)}`,
+    }),
   })
 }
