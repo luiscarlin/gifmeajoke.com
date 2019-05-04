e5505a97019b47eeb494a63483a0d6c80da43760
Luis Carlin <luiscarlin@gmail.com>
2019-05-04 09:19:00 -0400

    start styling card

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import GphApiClient from 'giphy-js-sdk-core'
-import fetchJoke from '../services/fetch-joke'
+import React from 'react'
+import JokeCard from './JokeCard'
 
 function App() {
-  const client = GphApiClient(process.env.GIPHY_KEY)
-  const [joke, setJoke] = useState('')
-  const [gif, setGif] = useState('')
-
-  const getJoke = () => {
-    fetchJoke().then(payload => {
-      setJoke(payload.joke)
-      setGif(payload.gifUrl)
-    })
-  }
-
-  useEffect(() => {
-    getJoke()
-  }, [])
-
-  return (
-    <div className="App">
-      <h1>"{joke}"</h1>
-      <iframe
-        src={gif}
-        width="480"
-        height="271"
-        frameBorder="0"
-        class="giphy-embed"
-        allowFullScreen
-      />
-      <p>
-        <a href={gif}>via GIPHY</a>
-      </p>
-      <button onClick={() => getJoke()}>Get New Joke</button>
-    </div>
-  )
+  return <JokeCard />
 }
 
 export default App
