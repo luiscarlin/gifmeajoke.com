3a92fc05d1c2cf04d5b8ee9f3cce0faf44265d66
Luis Carlin <luiscarlin@gmail.com>
2019-05-28 06:18:19 -0400

    refactor to organize test better

--- src/lambda/services/fetch-gif-service.test.js
+++ src/lambda/services/fetch-gif-service.test.js
@@ -4,21 +4,25 @@ import giphyJsSdkCore from 'giphy-js-sdk-core'
 jest.mock('giphy-js-sdk-core')
 
 describe('fetch gif service', () => {
-  const translateMock = jest.fn(() =>
-    Promise.resolve({
-      data: {
-        images: {
-          original: {
-            url: 'gif-url',
+  let translateMock
+
+  beforeEach(() => {
+    translateMock = jest.fn(() =>
+      Promise.resolve({
+        data: {
+          images: {
+            original: {
+              url: 'gif-url',
+            },
           },
         },
-      },
-    })
-  )
+      })
+    )
+
+    giphyJsSdkCore.mockReturnValue({ translate: translateMock })
+  })
 
   it('returns a gif url', async () => {
-    giphyJsSdkCore.mockReturnValue({ translate: translateMock })
-
     const result = await fetchGifService('some text')
     expect(result).toBe('gif-url')
   })
