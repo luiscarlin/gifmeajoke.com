b75f8bdf258daeb107ff3260fd5c5a583598f4e5
Luis Carlin <luiscarlin@gmail.com>
2019-06-17 20:35:58 -0400

    get swtich color correct

--- src/app.js
+++ src/app.js
@@ -41,7 +41,16 @@ const App = () => {
 
   return (
     <AppContainer style={fadeIn}>
-      <Switch onChange={toggleTheme} checked={toggle} />
+      <Switch
+        onChange={toggleTheme}
+        checked={toggle}
+        uncheckedIcon={false}
+        checkedIcon={false}
+        offColor="#000"
+        onColor="#fff"
+        boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+        activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+      />
       <Welcome />
       <Card />
       <Footer />
