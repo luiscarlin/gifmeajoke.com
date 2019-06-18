64cb37925371d7acee9639b4945579199e1b44e5
Luis Carlin <luiscarlin@gmail.com>
2019-06-17 21:07:56 -0400

    place the toggle

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-import React, { useContext, useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { useSpring, animated } from 'react-spring'
 import Card from './components/Card'
 import Footer from './components/Footer'
 import Welcome from './components/Welcome'
-import { ThemeContext } from './ThemeContext'
-import Switch from 'react-switch'
 
 const AppContainer = styled(animated.div)`
   display: flex;
@@ -25,32 +23,9 @@ const AppContainer = styled(animated.div)`
 
 const App = () => {
   const fadeIn = useSpring({ from: { opacity: 0 }, opacity: 1 })
-  const { theme, setTheme } = useContext(ThemeContext)
-
-  const [toggle, setToggle] = useState(false)
-
-  const toggleTheme = () => {
-    if (theme === 'light') {
-      setTheme('dark')
-      setToggle(true)
-    } else {
-      setTheme('light')
-      setToggle(false)
-    }
-  }
 
   return (
     <AppContainer style={fadeIn}>
-      <Switch
-        onChange={toggleTheme}
-        checked={toggle}
-        uncheckedIcon={false}
-        checkedIcon={false}
-        offColor="#000"
-        onColor="#fff"
-        boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-        activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-      />
       <Welcome />
       <Card />
       <Footer />
---
+++ src/components/ThemeToggle.js
@@ -0,0 +1,33 @@
+import React, { useState, useContext } from 'react'
+import { ThemeContext } from '../ThemeContext'
+import Switch from 'react-switch'
+
+const ThemeToggle = () => {
+  const { theme, setTheme } = useContext(ThemeContext)
+  const [toggle, setToggle] = useState(false)
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark')
+      setToggle(true)
+    } else {
+      setTheme('light')
+      setToggle(false)
+    }
+  }
+
+  return (
+    <Switch
+      onChange={toggleTheme}
+      checked={toggle}
+      uncheckedIcon={false}
+      checkedIcon={false}
+      offColor="#000"
+      onColor="#fff"
+      boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+      activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+    />
+  )
+}
+
+export default ThemeToggle
